eb5ec87a15cdfa2351edef4def93579968503e68
ejovo13 <ejovo13@users.noreply.github.com>
2021-10-19 11:37:11 +0000

    deploy: d76ab704e90b6d9f8fd327743c8b92e0672005c2

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['est_5fpalindrome_0',['est_palindrome',['../TD2_8h.html#a4fa21d70de58ebd0ad8dc8417a376de1',1,'est_palindrome(const int *__T, size_t __N):&#160;TD2.c'],['../TD5_8h.html#a4fa21d70de58ebd0ad8dc8417a376de1',1,'est_palindrome(const int *__T, size_t __N):&#160;TD2.c']]]
+  ['est_5fpalindrome_0',['est_palindrome',['../TD2_8h.html#a4fa21d70de58ebd0ad8dc8417a376de1',1,'est_palindrome(const int *__T, size_t __N):&#160;ex4.c'],['../TD5_8h.html#a4fa21d70de58ebd0ad8dc8417a376de1',1,'est_palindrome(const int *__T, size_t __N):&#160;ex4.c']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['crible_5ferastosthene_0',['crible_erastosthene',['../TD2_8h.html#aded8df99c7194a5cfdf4e0f4fd7ec632',1,'crible_erastosthene(size_t __N):&#160;TD2.c'],['../TD5_8h.html#aded8df99c7194a5cfdf4e0f4fd7ec632',1,'crible_erastosthene(size_t __N):&#160;TD2.c']]]
+  ['crible_5ferastosthene_0',['crible_erastosthene',['../TD2_8h.html#aded8df99c7194a5cfdf4e0f4fd7ec632',1,'crible_erastosthene(size_t __N):&#160;ex7.c'],['../TD5_8h.html#aded8df99c7194a5cfdf4e0f4fd7ec632',1,'crible_erastosthene(size_t __N):&#160;ex7.c']]]
 ];
